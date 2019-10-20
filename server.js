bb9f5593108594f0813031efb935ffebf81dd5f0
Martyn Green <martyn.robert.green@gmail.com>
2019-10-20 21:54:45 +0100

    updated based on working copy

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ var fs = require("fs");
 var jmespath = require('jmespath');
 var bodyParser = require('body-parser')
 var debug = false;
+let request = require('sync-request');
 var path = require('path')
 var channel;
 var admin;
 var auth = require('./JSON/auth.json')
 var adminID = require('./JSON/adminID.json')
+var allFactions = [];
+var eventsToListenFor = [];
 app.use(bodyParser.json());
 const bot = new Discord.Client();
 bot.login(auth.token);
@@ -20,16 +23,54 @@ bot.on('ready', () => {
     admin = bot.users.get(adminID.adminID);
     channel = admin
     channel.send(runningState)
-    
+
 });
 
+dirname = './factions/'
+fs.readdir(dirname, function (err, filenames) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    filenames.forEach(function (filename) {
+        fs.readFile(dirname + filename, 'utf-8', function (err, content) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            JSONcontent = JSON.parse(content).Actions
+            for (J = 0; J <= JSONcontent.length - 1; J++) {
+                if (JSONcontent[J].LocationsAreaOfEffect.toString().toLowerCase() != "") {
+                    eventsToListenFor.push(JSONcontent[J].Event)
+                    console.log(JSON.stringify(eventsToListenFor))
+                    addToLocations = [];
+                    for (S = 0; S <= JSONcontent[J].LocationsScope.length - 1; S++) {
+                        searchString = "https://www.edsm.net/api-v1/" + JSONcontent[J].LocationsShapeAreaOfEffect.toString().toLowerCase() + "-systems?systemName=" + JSONcontent[J].LocationsScope[S] + "&size=" + JSONcontent[J].LocationsAreaOfEffect
+                        var res = request('GET', searchString);
+                        response = JSON.parse(Buffer.from(res.body).toString())
+                        for (R = 0; R <= response.length - 1; R++) {
+                            addToLocations.push(response[R].name)
+                        }
+                    }
+                    //JSONcontent[J].LocationsScope = JSONcontent[J].LocationsScope.concat(addToLocations)
+                    //console.log(JSON.stringify(addToLocations))
+                    //return
+                }
+            }
+            allFactions.push(JSON.parse(content));
+        });
+    });
+});
+
+
+
 app.get('/version', function (req, res) {
-    res.end('0.0.2');
+    res.end('0.0.2b');
 })
 app.get('/download', function (req, res) {
     //currentVersion = fs.readFileSync('./load.py', 'utf8');
     var options = {
-        root: path.join(__dirname, './'),
+        root: path.join(__dirname, './Plugin'),
         dotfiles: 'deny',
         headers: {
             'x-timestamp': Date.now(),
@@ -46,8 +87,9 @@ app.get('/download', function (req, res) {
 })
 app.get('/load.py', function (req, res) {
     //currentVersion = fs.readFileSync('./load.py', 'utf8');
+    console.log(JSON.stringify(allFactions))
     var options = {
-        root: path.join(__dirname, './'),
+        root: path.join(__dirname, './Plugin'),
         dotfiles: 'deny',
         headers: {
             'x-timestamp': Date.now(),
@@ -62,21 +104,14 @@ app.get('/load.py', function (req, res) {
         }
     })
 })
-app.post('/events', function (req, res) {
-    event = req.body
-    filename = './JSON/events_' + event.timestamp.replace(/\:/g, "-") + '.json';
-    var responseString = "";
-    if (event.event == "MissionCompleted") {
-        for (f = 0; f <= event.FactionEffects.length - 1; f++) {
-            var faction = event.FactionEffects[f].Faction
-            if (faction == faction) {
-                
 
-                channel.sendMessage(responseString);
-                res.end(JSON.stringify("OK "));
-            }
-        }
-    } else if (event.event == "Docked") {
+app.post('/events', function (req, res) {
+    var event = req.body
+    filename = './JSON/events_' + event.timestamp.replace(/\:/g, "-") + '.json';
+
+    console.log("Event received " + req.body.event)
+
+    if (event.event == "Docked") {
         systems = require('./JSON/systems.json')
         CMDR = event.commandername
         searchString = '[? systemAddress == `' + event.SystemAddress + '`].StarSystem'
@@ -91,47 +126,94 @@ app.post('/events', function (req, res) {
                 if (err) throw err;
             });
         }
-        if (debug) {
-            responseString = CMDR + " has docked at " + event.StationName + " in " + event.StarSystem
-            var user = bot.users.get('353894762761289728');
-            user.sendMessage(responseString);
-        }
-        res.end(JSON.stringify("OK"));
-    } else if (event.event == "SellExplorationData" || event.event == "MultiSellExplorationData") {
-        CMDR = event.commandername
-        station = event.currentStation
-        system = event.currentSystem
-        responseString = CMDR + " has sold " + event.TotalEarnings + " CR with of Carto data at " + station + " in " + system
-
-
-        channel.sendMessage(responseString);
-        res.end(JSON.stringify("OK"));
-
-    } else if (debug && event.event == "Undocked") {
-        systems = require('./JSON/systems.json')
-        CMDR = event.commandername
-        responseString = CMDR + " has undocked from " + event.StationName
-        var user = bot.users.get('353894762761289728');
-        user.sendMessage(responseString);
-        res.end(JSON.stringify("OK"));
-    } else if (debug && event.event == "MissionAccepted") {
-        filename = './JSON/MissionCompleted_' + event.timestamp.replace(/\:/g, "-") + '.json';
-        res.end(JSON.stringify("OK"));
-    } else if (debug) {
-        filename = './JSON/Event_' + event.timestamp.replace(/\:/g, "-") + '.json';
-        fs.writeFile(filename, JSON.stringify(event), 'utf8', function (err) {
-            if (err) throw err;
-            console.log('File is created successfully.');
-            res.end(JSON.stringify("OK"));
-        });
-        res.end(JSON.stringify("OK"));
-    } else if(event.event == "Bounty"){
-
-    
-    } else {
-        res.end(JSON.stringify("Sure"));
     }
+
+    console.log(JSON.stringify(event))
+    searchString = '[*].Actions[? Event == `"' + event.event + '"`]'
+    var results = jmespath.search(allFactions, searchString)
+    sendMessage = false
+    // if (eventsToListenFor.includes(event.event)) {
+
+    for (a = 0; a <= results.length - 1; a++) {
+        console.log("Factions want to know about this event")
+        for (b = 0; b <= results[a].length - 1; b++) {
+            try {
+                who = results[a][b].Who //Who can report to this faction
+                if (who == "everyone"  ||  event.reportingFaction.includes(who)) { //Everyone or faction only
+                    LocationsToCheck = results[a][b].LocationsToCheck //position in event JSON location is at
+                    systemAddress = jmespath.search(event, LocationsToCheck) //assume is systemAddress
+                    resolveNameSearch = resolveSystemName(systemAddress) //then convert to System Name 
+                    if (results[a][b].LocationsScope.includes(resolveNameSearch)) { 
+                        try {
+                            message = replaceVariables(results[a][b].Message, event)
+                            server = results[a][b].ServerChannel
+                            when = results[a][b].When.toString().toLowerCase()
+                            sendMessaage(message, server, when)
+                        } catch (a) {
+                            console.log("Not send message action")
+                        }
+                        try {
+                            SaveDataBasedOn = results[a][b].SaveDataBasedOn
+                            saveString = ""
+                            for (s = 0; s <= SaveDataBasedOn.length - 1; s++) {
+                                saveString +=  replaceVariables(SaveDataBasedOn[s], event)
+                            }
+                            server = results[a][b].ServerChannel
+                            saveString += server
+                            
+                            console.log(saveString)
+                            
+                            
+                        } catch (a) {
+                            console.log("Not send message action")
+                        }
+                    }
+                }
+            }
+            catch (e) {
+                console.log("An error occured", e)
+            }
+        }
+    }
+    console.log("End")
+    res.end(JSON.stringify("OK"));
 })
+function replaceVariables(message, inputJSON) {
+
+    do {
+        if ((message.indexOf("$") == -1)) { break }
+
+        var variable = message.toString().match(/\$[^\s\,\|\/]*/)[0].replace("$", "")
+        var searchString = "@." + variable
+        var results = jmespath.search(inputJSON, searchString)
+        if (results == null) { continue }
+        results = (resolveSystemName(results) || results)
+        message = message.replace("$" + variable, results)
+        console.log(message)
+
+    } while (message.indexOf("$") > 0)
+
+
+    return message
+
+}
+
+function resolveSystemName(systemAddress) {
+    systems = require('./JSON/systems.json')
+    resolveNameSearch = '[? systemAddress == `' + systemAddress + '` ||  StarSystem == `"' + systemAddress + '"`].StarSystem'
+    system = jmespath.search(systems, resolveNameSearch)[0]
+    return system || systemAddress;
+}
+
+function sendMessage(message, server, when) {
+    if (when == "now") {
+        console.log("sending now")
+        console.log(message)
+        channel = bot.channels.get(server);
+        channel.sendMessage(message);
+    }
+}
+
 var port = process.env.PORT || 80
 var server = app.listen(port, function () {
     var host = server.address().address
